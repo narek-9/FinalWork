30a8382822599ca248e02de86ed19bee17ea87eb
Narek <narekazibekyan5@gmail.com>
2022-08-10 20:26:49 +0400

    Written the task solution.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿string[] firstArray = { "World", "!", "C#", "Hello", ":)", "Programming", "324" };
+
+Random rnd = new Random();
+string[] secondArray = new string[rnd.Next(0, 4)];
+
+// Заполнение нового массива значениями из старого.
+
+for (int i = 0; i < firstArray.Length; i++) // Пробегаемся по эдементам исходного масиива.
+{
+    for (int j = 0; j < secondArray.Length; j++) // Пробегаемся по позициям нового масиива.
+    {
+        secondArray[j] = firstArray[rnd.Next(firstArray.Length)];
+    }
+}
+
+Console.WriteLine("[" + string.Join(", ", secondArray) + "]");
+
